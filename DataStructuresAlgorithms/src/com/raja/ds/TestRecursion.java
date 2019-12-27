bf3c957675dac92346e39781faed733723391959
javaterminology <vrajasekharreddy@gmail.com>
2019-12-28 00:06:20 +0800

    test recursion

--- DataStructuresAlgorithms/src/com/raja/ds/TestRecursion.java
+++ DataStructuresAlgorithms/src/com/raja/ds/TestRecursion.java
@@ -1,10 +1,19 @@
 package com.raja.ds;
 
-public class Test {
+public class TestRecursion {
 
+	public static void foo(int n){
+		if(n<1){
+			return;
+		}else{
+			foo(n-1);
+		}
+		System.out.println("Hello foo! "+n);
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		foo(3);
+		System.out.println("Recursion completed!");
 	}
 
 }
